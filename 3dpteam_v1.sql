43bfd02b96ab4c30a8f2f3cd20e342d33a0c4a4f
kprses <kprses1@etf.unsa.ba>
2016-05-18 16:07:03 +0200

    baza v1.1

--- 3dpteam_v1.sql
+++ 3dpteam_v1.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `3dpteam` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `3dpteam`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: 3dpteam
@@ -110,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-18 16:01:17
+-- Dump completed on 2016-05-18 16:06:37
