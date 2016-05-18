95ce6aea0105a2c7bc202deb7d3dbbe3c077051b
kprses <kprses1@etf.unsa.ba>
2016-05-18 15:46:06 +0200

    servis za pozivni broj

---
+++ js/pozivniServis.js
@@ -0,0 +1,23 @@
+function dobaviPozivniBroj(dvoslovniKod)
+{
+    var ajax = new XMLHttpRequest();
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200)
+        {
+
+            var json = ajax.responseText;
+            var pozivniBroj = JSON.parse(json);
+            var pozivniBrojDrzave = pozivniBroj[0].callingCodes;
+
+            document.getElementById("brojTelefona").innerHTML = "(+" + pozivniBrojDrzave + ")";
+        }
+            
+        if (ajax.readyState == 4 && ajax.status == 404)
+            alert("GREŠKA!!! Molimo pokušajte kasnije.");
+            
+    }
+
+    ajax.open("GET", "https://restcountries.eu/rest/v1/alpha?codes=" + dvoslovniKod, true);
+    ajax.send();
+}
